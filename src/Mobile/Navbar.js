d54b9ec5300c15cf40a44cbb983aca7ebf3dfb6c
ernestsmotlak <ernestsmotlak@gmail.com>
2024-06-25 17:42:26 +0200

    Installed bootstrap
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

---
+++ src/Mobile/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+      <nav class="navbar fixed-top bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Fixed top
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
