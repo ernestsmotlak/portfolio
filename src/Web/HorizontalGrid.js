7e12a374dd0386e1dabd8f3d56742fc6efc7d833
ernestsmotlak <ernestsmotlak@gmail.com>
2024-07-09 12:43:53 +0200

    Added a folder of images.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/Web/HorizontalGrid.js
+++ src/Web/HorizontalGrid.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const HorizontalGrid = () => {
   return (
-    <div>HorizontalGrid</div>
-  )
-}
+    <div
+      style={{ height: "600px", paddingTop: "5rem" }}
+    //   className="bg-info-subtle"
+    >
+        Technologies
+      <div class="container text-center border" style={{height: '300px', marginTop: '2rem'}}>
+        <div class="row" style={{marginTop: '2rem'}}>
+          <div class="col">
+          <img src="..." class="img-fluid" alt="..."></img>
+          </div>
+          <div class="col">
+          <img src="..." class="img-fluid" alt="..."></img>
+          </div>
+          <div class="col">
+          <img src="..." class="img-fluid" alt="..."></img>
+          </div>
+        </div>
+        <div class="row" style={{marginTop: '10rem'}}>
+          <div class="col">
+          <img src="..." class="img-fluid" alt="..."></img>
+          </div>
+          <div class="col">
+          <img src="..." class="img-fluid" alt="..."></img>
+          </div>
+          <div class="col">
+          <img src="..." class="img-fluid" alt="..."></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HorizontalGrid
+export default HorizontalGrid;
