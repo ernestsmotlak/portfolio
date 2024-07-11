97c25180802be69de41ac724e4365f37086ab62e
ernestsmotlak <ernestsmotlak@gmail.com>
2024-07-11 09:52:32 +0200

    Additional things, nevem.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/Web/Projects.js
+++ src/Web/Projects.js
@@ -3,27 +3,90 @@ import React from "react";
 const Projects = () => {
   return (
     <div>
-      My Projects
-      <br />
-      <div class="container text-center">
-        <div class="row">
-            Full Stack Projects
-          <div class="col">Chat App</div>
-        </div>
-        <div class="row">
-          <div class="col">Weather App</div>
-        </div>
-        <div class="row">
-          <div class="col">Timetable</div>
-        </div>
-        <div class="row">
-          <div class="col">To Do App</div>
-        </div>
-        Frontend Projects
-        <div class="row">
-          <div class="col">Hello World</div>
-        </div>
+      <div
+        className="fs-2 text-center"
+        style={{ marginTop: "5rem", marginBottom: "2rem" }}
+      >
+        FULL STACK PROJECTS
       </div>
+      <br />
+      <ol
+        class="list-group list-group-numbered mx-auto"
+        style={{ width: "30rem" }}
+      >
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Chat App</div>
+            Dynamic real-time messaging app.
+            <ul>
+              <li>React</li>
+              <li>Node.js</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Timetable App</div>
+            Bus transport app for the city of Koper.
+            <ul>
+              <li>React</li>
+              <li>Node.js</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">To Do App</div>
+            Task management tool for organizing your daily tasks.
+            <ul>
+              <li>React</li>
+              <li>Node.js</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+        </li>
+      </ol>
+
+      <div
+        className="fs-2 text-center"
+        style={{ marginTop: "5rem", marginBottom: "2rem" }}
+      >
+        FRONTEND PROJECTS
+      </div>
+      <br />
+      <ol
+        class="list-group list-group-numbered mx-auto"
+        style={{ width: "30rem" }}
+      >
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Subheading</div>
+            Content for list item
+          </div>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Subheading</div>
+            Content for list item
+          </div>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">Subheading</div>
+            Content for list item
+          </div>
+          <span class="badge text-bg-primary rounded-pill">14</span>
+        </li>
+      </ol>
     </div>
   );
 };
