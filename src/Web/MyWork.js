f320c5337ce985b43c541bec530e7418425c9893
ernestsmotlak <ernestsmotlak@gmail.com>
2024-07-12 15:39:35 +0200

    next work on vertical grid
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/Web/MyWork.js
+++ src/Web/MyWork.js
@@ -3,21 +3,22 @@ import { GiLaptop } from "react-icons/gi";
 
 const MyWork = () => {
   return (
-    <div style={{ height: "500px" }} className="bg-secondary-subtle">
+    <div className="bg-secondary-subtle pb-5">
       <div class="container text-center" style={{ paddingTop: "3rem" }}>
         <div class="row">
-          <div className="col mb-5 fs-1">My Work</div>
-          <div class="row">
-            <GiLaptop style={{ fontSize: "10rem", marginLeft: "10px" }} />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col">
+          <div className="col mb-4 fs-2">
+            MY WORK
             <hr
               className="mx-auto"
               style={{ border: "2.5px solid", width: "15rem" }}
             ></hr>
           </div>
+          <div class="row">
+            <GiLaptop style={{ fontSize: "10rem", marginLeft: "10px" }} />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col"></div>
         </div>
         <div class="row">
           <div class="col mx-auto mt-3" style={{ maxWidth: "40rem" }}>
