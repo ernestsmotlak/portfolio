49c250bd3f6755dc94141c3faaa401fcc38e1da0
ernestsmotlak <ernestsmotlak@gmail.com>
2024-07-15 20:54:31 +0200

    Added some changes to Description.js, removed the ps and pb
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/Web/Description.js
+++ src/Web/Description.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Description = () => {
   return (
-    <div className="pt-5 pb-5">
+    <div className="pt-4">
       <div className="fs-1">PORTFOLIO WEBSITE</div>
       <div className="fs-2">ERNEST SMOTLAK</div>
     </div>
