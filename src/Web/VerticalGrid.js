9650c418be7f6fec7eb3cc6d8c2c3b2a587a31d6
ernestsmotlak <ernestsmotlak@gmail.com>
2024-07-16 11:00:38 +0200

    changed padding bottom of the background
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/Web/VerticalGrid.js
+++ src/Web/VerticalGrid.js
@@ -11,7 +11,7 @@ const VerticalGrid = () => {
         width: "100%",
         margin: "0 auto",
         paddingTop: "5rem",
-        paddingBottom: "6rem",
+        paddingBottom: "5rem",
       }}
     >
       <div
